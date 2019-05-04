73753ab6637ea47f7d929e8e4d96b23689450fb8
ŁukaszLampart <ll@ll.ll>
2019-05-04 17:28:35 +0200

    "Almost" integrated with socket-io

--- src/main/scala/pl/edu/agh/gwent/ai/client/SocketIOEvents.scala
+++ src/main/scala/pl/edu/agh/gwent/ai/client/SocketIOEvents.scala
@@ -1,12 +1,17 @@
 package pl.edu.agh.gwent.ai.client
 
-import cats.effect.IO
+import cats.effect.{ContextShift, IO, Resource}
 import com.avsystem.commons.serialization.GenCodec
 import fs2._
-import io.socket.client.Socket
+import fs2.concurrent.Queue
+import io.socket.client.{Socket, IO => SIO}
+import io.socket.emitter.Emitter
 
-class SocketIOEvents[C, U] (
+import scala.concurrent.ExecutionContext
+
+class SocketIOEvents[C, U](
   socket: Socket,
+  eventsQueue: Queue[IO, U],
   eventNameF: C => String
 )(implicit
   commandCodec: GenCodec[C],
@@ -18,10 +23,40 @@ class SocketIOEvents[C, U] (
   override def publish(commands: Stream[IO, C]): IO[Unit] =
     commands.evalMap(c => IO(socket.emit(eventNameF(c), encode(c)))).compile.drain
 
-  override def events: Stream[IO, U] = ???
+  override def events: Stream[IO, U] = eventsQueue.dequeue
 }
 
 
 object SocketIOEvents {
+
+  def make[C, U](
+    uri: String,
+    events: List[String],
+    eventNameF: C => String
+  )(implicit
+    commandCodec: GenCodec[C],
+    updateCodec: GenCodec[U],
+    cs: ContextShift[IO]
+  ): Resource[IO, EventStream[IO, Stream[IO, ?], C, U]] = {
+
+    def decode(arg: AnyRef): U = ???
+
+    def create(queue: Queue[IO, U]) = IO {
+      val sock = SIO.socket(uri)
+      events.foldLeft(sock: Emitter) { (s, ev) =>
+        s.on(ev, (args: Array[AnyRef]) => queue.enqueue1(decode(args(0))).unsafeRunSync())
+      }
+      sock.connect()
+    }
+
+    for {
+      queue <- Resource.liftF(Queue.bounded[IO, U](1000))
+      sock <- Resource.make(create(queue))(s => IO(s.close()))
+    } yield new SocketIOEvents(sock, queue, eventNameF)
+  }
+
+  private implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+
+  val example = make[String, String]("localhost", List("ev1", "ev2", "ev3"), identity)
 
 }
